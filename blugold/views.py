fd4d2299848c36a332d2ac025d16235c797c1f85
Rob Gannon <cyberpeaches@gmail.com>
2022-08-06 20:13:54 +0000

    set up views for crud operations

--- blugold/views.py
+++ blugold/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from rest_framework import viewsets, generics
+from django.views import View
+from django.http import HttpResponse, HttpResponseNotFound
+import os       
+from .serializers import StationSerializer     
+from .models import Station                     
 
-# Create your views here.
+class BlugoldView(viewsets.ModelViewSet):
+    serializer_class = StationSerializer          
+    queryset = Station.objects.all()
+
+class Assets(View):
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
+
+class StationCreate(generics.CreateAPIView):
+    # API endpoint that allows creation of a new station
+    serializer_class = StationSerializer
+    queryset = Station.objects.all(),
+
+class StationList(generics.ListAPIView):
+    # API endpoint that allows station to be viewed.
+    serializer_class = StationSerializer
+    queryset = Station.objects.all()
+
+class StationDetail(generics.RetrieveAPIView):
+    # API endpoint that returns a single station by id.
+    serializer_class = StationSerializer
+    queryset = Station.objects.all()
+
+class StationUpdate(generics.RetrieveUpdateAPIView):
+    # API endpoint that allows a Station record to be updated.
+    serializer_class = StationSerializer
+    queryset = Station.objects.all()
+
+class StationDelete(generics.RetrieveDestroyAPIView):
+    # API endpoint that allows a Station record to be deleted.
+    serializer_class = StationSerializer
+    queryset = Station.objects.all()
